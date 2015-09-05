8bb0d999d2a54a7e5ce943f489422817087fc6e9
xPBx <patrickbchir@gmail.com>
2015-09-05 18:13:10 +0300

    Initial commit

---
+++ app/src/main/java/com/db/witzexxl/MainActivity.java
@@ -0,0 +1,30 @@
+package com.db.witzexxl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+    }
+    public void buttonstart(View view) {
+        setContentView(R.layout.witz01);
+    }
+}
